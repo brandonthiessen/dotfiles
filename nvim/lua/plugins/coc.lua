6b79ad650446c82603776ea0f95b877da37082e6
brandonthiessen <brandonthiessen2005@gmail.com>
2026-04-22 14:16:19 -0400

    Streamlined completion usage with TAB to confirm

--- nvim/lua/plugins/coc.lua
+++ nvim/lua/plugins/coc.lua
@@ -13,8 +13,14 @@ return {
             \ coc#pum#visible() ? coc#pum#prev(1) : "\<C-k>"
             ]])
 
-            -- Trigger completion manually
+            -- Trigger completion with Ctrl-Space
             vim.cmd([[ inoremap <silent><expr> <C-Space> coc#refresh() ]])
+
+            -- Confirm completion manually with TAB
+            vim.cmd([[
+            inoremap <silent><expr> <TAB>
+            \ coc#pum#visible() ? coc#pum#confirm() : "\<Tab>"
+            ]])
 
             -- Basic navigation
             vim.keymap.set("n", "[g", "<Plug>(coc-diagnostic-prev)", { silent = true })
