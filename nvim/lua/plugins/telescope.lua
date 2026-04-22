73a6b592cfdabf471ba5e91b62425987b885a0f7
brandonthiessen <brandonthiessen2005@gmail.com>
2026-04-22 13:48:11 -0400

    Include hidden files in find_files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -25,7 +25,12 @@ return {
             })
 
             -- Leader key mappings for common pickers
-            vim.keymap.set("n", "<leader>ff", builtin.find_files)
+            vim.keymap.set("n", "<leader>ff", function()
+                builtin.find_files({
+                    hidden = true,
+                    no_ignore = true,
+                })
+            end)
             vim.keymap.set("n", "<leader>fg", builtin.live_grep)
             vim.keymap.set("n", "<leader>fb", builtin.buffers)
             vim.keymap.set("n", "<leader>fh", builtin.help_tags)
